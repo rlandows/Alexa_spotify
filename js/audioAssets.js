e73f59de726b54a05b881a6b7a9703e50b976aff
rlandows <rlandows@uci.edu>
2017-04-03 12:21:26 -0700

    pushing audio asset file

--- js/audioAssets.js
+++ js/audioAssets.js
@@ -1,45 +1,11 @@
-module.exports = new Promise((resolve, request) => {
-var song_uri = ['7qiZfU4dY1lWllzX7mPBI3', '17cy40GIhqCU5aQzlSI5EX', '6AeQlMyRzvSl1nkFztZyKl']
+'use strict';
 
-var request = require('request');
-var i = 0;
-var json_list = [];
-while (i < song_uri.length) {
+// Audio Source - AWS Podcast : https://aws.amazon.com/podcasts/aws-podcast/
+var audioData = [ { title: 'Shape of You',
+    url: 'https://p.scdn.co/mp3-preview/84462d8e1e4d0f9e5ccd06f0da390f65843774a2?cid=null' },
+  { title: 'Our House',
+    url: 'https://p.scdn.co/mp3-preview/c509ee339241db2d6f451505048b4c5acf4c6587?cid=null' },
+  { title: 'Issues',
+    url: 'https://p.scdn.co/mp3-preview/29226b85b506f3e250ed8aab9fa3403b766213dd?cid=null' } ];
 
-
-
-
-var response = request({
-    method: 'GET',
-    uri: 'https://api.spotify.com/v1/tracks/' + song_uri[i]
-}, function(error, response, body) {
-    //* workaround for issue with this particular apiUrl
-    var firstChar = body.substring(0, 1);
-    var firstCharCode = body.charCodeAt(0);
-    if (firstCharCode == 65279) {
-        console.log('First character "' + firstChar + '" (character code: ' + firstCharCode + ') is invalid so removing it.');
-        body = body.substring(1);
-    }
-        
-        var parsedJson = JSON.parse(body).preview_url;
-        var titleJson = JSON.parse(body).name;
-    
-        json_list.push({title:titleJson, url: parsedJson});
- 
-//        console.log(json_list);
-        if (json_list.length == song_uri.length ) {
-            resolve(json_list);
-            console.log(json_list);
-        }
-//    console.log('parsedJson: ', parsedJson);
-    
-    });
-    i++;
-//    if (i === json_list.length - 1) {
-//    return json_list;
-//    };
-};
-
-console.log(json_list);
-console.log(i);
-});
+module.exports = audioData;
---
+++ js/index 2.js/audioAssets.js
@@ -0,0 +1,45 @@
+module.exports = new Promise((resolve, request) => {
+var song_uri = ['7qiZfU4dY1lWllzX7mPBI3', '17cy40GIhqCU5aQzlSI5EX', '6AeQlMyRzvSl1nkFztZyKl']
+
+var request = require('request');
+var i = 0;
+var json_list = [];
+while (i < song_uri.length) {
+
+
+
+
+var response = request({
+    method: 'GET',
+    uri: 'https://api.spotify.com/v1/tracks/' + song_uri[i]
+}, function(error, response, body) {
+    //* workaround for issue with this particular apiUrl
+    var firstChar = body.substring(0, 1);
+    var firstCharCode = body.charCodeAt(0);
+    if (firstCharCode == 65279) {
+        console.log('First character "' + firstChar + '" (character code: ' + firstCharCode + ') is invalid so removing it.');
+        body = body.substring(1);
+    }
+        
+        var parsedJson = JSON.parse(body).preview_url;
+        var titleJson = JSON.parse(body).name;
+    
+        json_list.push({title:titleJson, url: parsedJson});
+ 
+//        console.log(json_list);
+        if (json_list.length == song_uri.length ) {
+            resolve(json_list);
+            console.log(json_list);
+        }
+//    console.log('parsedJson: ', parsedJson);
+    
+    });
+    i++;
+//    if (i === json_list.length - 1) {
+//    return json_list;
+//    };
+};
+
+console.log(json_list);
+console.log(i);
+});
